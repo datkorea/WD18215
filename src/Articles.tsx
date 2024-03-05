2fb3987b7567b128482820928486a73509d1bf3b
datkorea <datkidonguyen2003@gmail.com>
2024-03-06 02:40:22 +0700

    baitapB1-2

--- src/Articles.tsx
+++ src/Articles.tsx
@@ -3,13 +3,15 @@ import "./index.css";
 import React from "react";
 import Header from "./component/lab1/Header";
 import Footer from "./component/lab1/Footer";
-import Banner from "./component/Banner";
+import Banner from "./component/lab1/Banner";
+import Content from "./component/lab1/Content";
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <div>
     <Header />
     <Banner />
+    <Content />
     <Footer />
   </div>
 );
---
+++ src/component/lab1/Banner.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Banner() {
+  return (
+    <div className="banner">
+        <img src="/src/component/lab1/img/b6b51422cb62ed996667e01bf108ab96.png" alt="" />
+      </div>
+  );
+}
+
+export default Banner;
